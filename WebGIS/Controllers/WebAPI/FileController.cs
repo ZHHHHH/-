a41934f5ecff3df2cb236f6267b53bbfb5893776
ZH、 <1015300310@qq.com>
2020-01-04 17:08:36 +0800

    添加File控制器
    Signed-off-by: ZH、 <1015300310@qq.com>

---
+++ WebGIS/Controllers/WebAPI/FileController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebGIS.ProviderClass;
+using WebGIS.ToolClass;
+
+namespace WebGIS.Controllers.WebAPI
+{
+    [Authorize]
+    [Route("[controller]/[action]")]
+    public class FileController : ControllerBase
+    {
+        [HttpPost]
+        /// <summary>
+        /// 读取字母表excel文件
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult LoadExcelFile()
+        {
+            try
+            {
+                string excelPath = HostEnvironmentProvider.ContentPath + "/常用业务词汇拼音首字母字典.xls";
+                DataFromExcel dataFromExcel = new DataFromExcel(excelPath);
+                DataTable dataTable = dataFromExcel.GetSheetData("Sheet1");
+                return new JsonResult(new { code = 0, data = dataTable });
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new { code = 1, data = ex.Message });
+            }
+        }
+    }
+}
